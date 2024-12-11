8e38b0ac57ef10f22b03be3ae1712f011001e40d
Ethan Sitnek <edjsitnek@gmail.com>
2024-12-11 14:42:11 -0500

    improve keyboard display

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './Keyboard.css'
 
 // An on-screen keyboard for typing guesses
 export default function Keyboard() {
-  const keys = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+  const [showNumKeys, setShowNumKeys] = useState(false); // Switch between letter and number/special character keyboards
+  const letterKeys = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+  const numChars = "1234567890!?&-'.()".split("");
 
   // Fills in letter keys to keyboard rows according to slice start and end parameters
-  const fillLetterKeys = (start, end) => {
+  const fillKeys = (keys, start, end) => {
     return (
       keys.slice(start, end).map((key, i) => (
         <button key={i} className={"key"} >
@@ -17,21 +20,42 @@ export default function Keyboard() {
 
   return (
     <div className="keyboard">
-      <div className="keyboard-row">
-        {fillLetterKeys(0, 10)}
-      </div>
-      <div className="keyboard-row">
-        {fillLetterKeys(10, 19)}
-      </div>
-      <div className="keyboard-row">
-        <button className="key key-large">
-          123*
-        </button>
-        {fillLetterKeys(19)}
-        <button className="key key-large">
-          ⌫
-        </button>
-      </div>
-    </div>
+      {!showNumKeys ? ( // Keyboard display with letter keys
+        <>
+          <div className="keyboard-row">
+            {fillKeys(letterKeys, 0, 10)}
+          </div>
+          <div className="keyboard-row">
+            {fillKeys(letterKeys, 10, 19)}
+          </div>
+          <div className="keyboard-row">
+            <button className="key key-large" onClick={() => setShowNumKeys(true)}>
+              123*
+            </button>
+            {fillKeys(letterKeys, 19)}
+            <button className="key key-large">
+              ⌫
+            </button>
+          </div>
+        </>
+      ) : ( // Keyboard display with numbers and special characters
+        <>
+          <div className="keyboard-row">
+            {fillKeys(numChars, 0, 10)}
+          </div>
+          <div className="keyboard-row">
+            {fillKeys(numChars, 10)}
+          </div>
+          <div className="keyboard-row">
+            <button className="key key-large" onClick={() => setShowNumKeys(false)} >
+              ABC
+            </button>
+            <button className="key key-large">
+              ⌫
+            </button>
+          </div>
+        </>
+      )}
+    </div >
   );
 };
