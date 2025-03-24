20c98c3516a6bcd09f82860632f48f64496d3996
Ethan Sitnek <edjsitnek@gmail.com>
2025-03-24 19:48:00 -0400

    fix upload songs script

--- backend/scripts/uploadSongsToKV.js
+++ backend/scripts/uploadSongsToKV.js
@@ -1,11 +1,8 @@
-import { readFile } from "fs/promises";
 import { execSync } from "child_process";
-
-const songs = await readFile("./songs.json", "utf-8");
 
 // Upload the songs.json to KV
 try {
-  execSync(`npx wrangler kv key put --binding=SONG_CACHE songs '${songs}'`, {
+  execSync("npx wrangler kv key put --binding=SONG_CACHE songs --path=./songs.json", {
     stdio: "inherit"
   });
 } catch (err) {
